a2c3122050e29b0c27371b399d1e7085a318a8f1
Deepak choudhary <technicaldeepak15@gmail.com>
2026-04-02 15:34:04 +0530

    update

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ toggle.addEventListener("click", () => {
 
   toggle.classList.toggle("fa-eye-slash");
 });
+
+
+
+
+
+
+const toggle = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+toggle.addEventListener("click", () => {
+  const type = password.type === "password" ? "text" : "password";
+  password.type = type;
+
+  toggle.classList.toggle("fa-eye-slash");
+});
