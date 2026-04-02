f097278ba2ef5a72353899583a096659cfcfedfd
Deepak choudhary <technicaldeepak15@gmail.com>
2026-04-02 15:18:36 +0530

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggle = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+toggle.addEventListener("click", () => {
+  const type = password.type === "password" ? "text" : "password";
+  password.type = type;
+
+  toggle.classList.toggle("fa-eye-slash");
+});
